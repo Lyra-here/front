25d3b5fd35aa393363ff15b33827113da947d098
Lyra <791041561@qq.com>
2022-07-06 12:57:09 +0800

    save it

---
+++ src/page/headline.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="fillcontain">
+    <head-top></head-top>
+    <article v-loading="loading">
+      <h1 class="article-title">{{ content.ArticleTitle }}</h1>
+      <img class="article-cover-image" :src="content.ArticleCoverImageUrl" />
+      <div class="author-container">
+        <p class="author-name">文章来源：{{ content.AuthorName }}</p>
+        <img class="author-head-image" :src="content.AuthorHeadImageUrl" />
+      </div>
+      <div class="divider"></div>
+      <div class="article-content" v-html="content.ArticleContentWithTags"></div>
+    </article>
+<el-pagination
+  small
+  layout = "prev, pager, next"
+  :page-size = pagesize
+  :page-count = pagecount
+  :current-page = pageindex
+  @current-change="getInfo()" >
+</el-pagination>
+  </div>
+</template>
+<script>
+import headTop from "../components/headTop";
+import { getHeadLine } from "../api/getData.js";
+export default {
+  data() {
+    return {
+      pagesize:1,//每页条目数
+      pagecount:0,//页数，Ajax请求接收后被改变
+      pageindex: 1, //页数从一开始
+      resReciver: new Map(),
+      content: {},  //初始为空对象，Ajax请求接收后被改变
+      loading: true,
+    };
+  },
+  created() {
+    this.getInfo(this.pageindex)
+  },
+  components: {
+    headTop,
+  },
+  methods: {
+    getInfo(pageI) {
+      getHeadLine().then((res) => {
+        console.log('raw Ajax response display',res);
+        this.resReciver = res;
+        this.content =  this.resReciver.data.Data[pageI-1];//Ajax获取数组内容，数组下标从0开始，所以减一
+        this.loading = false;
+        this.pagecount = res.data.Data.length;
+        console.log(
+          "this.resReciver",
+          this.resReciver,
+          "this.content",
+          this.content,
+          "pageI",
+          pageI,
+          "this.totalpage",
+          this.totalpage,
+        );
+      });
+    },
+    sayhi(){
+      console.log('hi',this.pageindex)
+    }
+  },
+};
+</script>
+<style lang="less">
+body,
+div,
+span,
+header,
+footer,
+nav,
+section,
+aside,
+article,
+ul,
+dl,
+dt,
+dd,
+li,
+a,
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+i,
+b,
+textarea,
+button,
+input,
+select,
+figure,
+figcaption {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  font-style: normal;
+  text-decoration: none;
+  border: none;
+  font-family: "Microsoft Yahei";
+}
+.el-pagination{
+  text-align: center;
+}
+p {
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+}
+.rich_media_content {
+  visibility: visible !important;
+}
+h1 {
+  font-weight: 400;
+  text-align: center;
+  font-size: 36px;
+  margin-bottom: 16px;
+  line-height: 1.35;
+}
+article {
+  display: flex;
+  flex-direction: column;
+  padding: 40px 90px 40px 90px;
+}
+::-webkit-scrollbar {
+  width: auto;
+  height: auto;
+  color: grey;
+  background-color: #f5f5f5;
+}
+title {
+  display: block;
+  font-size: 62px;
+}
+.divider {
+  width: 100%;
+  height: 1px;
+  border-bottom: 1px #222f3a solid;
+  margin: 20px 0 20px 0;
+}
+body,
+html {
+  width: 100%;
+  height: 100%;
+  font: 12px/20px Hiragino Sans GB, Microsoft Yahei, Arial;
+}
+.author-container {
+  text-align: right;
+}
+span {
+  font-size: 16px;
+  word-break: break-word;
+}
+</style>
